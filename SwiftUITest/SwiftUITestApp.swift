25af15cf8dfe3e135f061cad413a378a0c92e00f
Vipin <33759367+CodeVip@users.noreply.github.com>
2021-03-19 18:33:19 +0530

    initial commit

---
+++ SwiftUITest/SwiftUITestApp.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUITestApp.swift
+//  SwiftUITest
+//
+//  Created by vipin on 2/18/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
